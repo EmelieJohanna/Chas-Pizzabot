bf8a27d8293720931edbae60827db0f8d6b4f706
EmelieJohanna <emelielundbeerg@gmail.com>
2023-11-14 01:35:34 +0100

    updated

--- pizzabot.js
+++ pizzabot.js
@@ -6,64 +6,36 @@ const pizzaPrice = 80;
 
 //Put your Javscript code here:
 
-// Adress the user, tell them the menu.
-alert(
-  "Hey! Happy to serve your pizza. On our menu we have " +
-    vegetarian +
-    ", " +
-    hawaiian +
-    " , and " +
-    pepperoni
-);
-
-// Ask what pizza they want.
-// const orderName = prompt(
-//   "Enter the name of the pizza you want to order today."
-// );
-
-// Ask user how many pizzas.
-// const orderQuantity = prompt("How many of", orderName, "do you want?");
-
-// Check if input orderName matches any of the pizza variables. three if-statements
-
 // FUNCTION 1!
-
-let orderName = prompt("Enter the name of the pizza you want to order today.");
-
+// Check if input orderName matches any of the pizza variables. three if-statements
 function checkOrderName(orderName) {
+  orderName = orderName.toLowerCase();
   if (
-    orderName.toLowerCase() === "vego" ||
-    orderName.toLowerCase() === "vegetarian" ||
-    orderName.toLowerCase() === "vegetarian pizza"
+    orderName == "vego" ||
+    orderName == "vegetarian" ||
+    orderName == "vegetarian pizza"
   ) {
     return vegetarian;
   } else if (
-    orderName.toLowerCase() === "hawaiian" ||
-    orderName.toLowerCase() === "hawaii" ||
-    orderName.toLowerCase() === "hawaiian pizza"
+    orderName == "hawaiian" ||
+    orderName == "hawaii" ||
+    orderName == "hawaiian pizza"
   ) {
     return hawaiian;
   } else if (
-    orderName.toLowerCase() === "pepperoni" ||
-    orderName.toLowerCase() === "pepper" ||
-    orderName.toLowerCase() === "pepperoni pizza"
+    orderName == "pepperoni" ||
+    orderName == "pepper" ||
+    orderName == "pepperoni pizza"
   ) {
     return pepperoni;
   } else {
-    return checkOrderName(
-      console.log(
-        "Sorry, we don't have that pizza. Please pick one from our menu."
-      )
-    );
+    alert("Sorry, we don't have that pizza. Please pick one from our menu.");
   }
 }
 
 // FUNCTION 2 !
 // Calculation for total order price.
-
-let orderQuantity = prompt("How many of " + orderName + " do you want?");
-
-function totalCost() {
+function totalCost(pizzaPrice, orderQuantity) {
   const numberOrderQuantity = parseFloat(orderQuantity);
 
   if (isNaN(numberOrderQuantity)) {
@@ -73,14 +45,14 @@ function totalCost() {
   }
 }
 
+// FUNCTION 3
 // Calculate cooking time.
 //1-2 pizzas: The pizzas will take 10 minutes.
 //3-5 pizzas: The pizzas will take 15 minutes.
 //6+ pizzas: The pizzas will take 20 minutes.
 
-// FUNCTION 3
-
 function cookingTime(orderQuantity) {
+  let time;
   if (orderQuantity <= 3) {
     time = 10;
   } else if (orderQuantity >= 6) {
@@ -91,8 +63,7 @@ function cookingTime(orderQuantity) {
   return time;
 }
 
-//singular or plural?
-
+//singular or plural value?
 function sinOrPlu(orderQuantity) {
   if (orderQuantity > 1) {
     noun = "pizzas";
@@ -104,19 +75,38 @@ function sinOrPlu(orderQuantity) {
 
 // KALLA PÅ DOM
 
+// Adress the user, tell them the menu.
+alert(
+  "Hey! Happy to serve your pizza. On our menu we have " +
+    vegetarian +
+    ", " +
+    hawaiian +
+    ", and " +
+    pepperoni
+);
+
+// Ask what pizza they want.
+let orderName = prompt("Enter the name of the pizza you want to order today.");
+console.log("OrderName: ", orderName);
 let orderType = checkOrderName(orderName);
-let total = totalCost();
+
+// Ask user how many pizzas.
+let orderQuantity = prompt("How many of " + orderType + " do you want?");
+
+let total = totalCost(pizzaPrice, orderQuantity);
 let time = cookingTime(orderQuantity);
-let sinOrPlu = sinOrPlu(orderQuantity);
+console.log(cookingTime);
+let singularOrPlural = sinOrPlu(orderQuantity);
 
 let finalMessage =
   "Great, I'll get started on your " +
-  orderName +
+  orderType +
   " right away, it will cost " +
-  totalCost +
+  total +
   " kr. The " +
-  noun +
+  singularOrPlural +
   " will take " +
   time +
   "minutes.";
+
 alert(finalMessage);
